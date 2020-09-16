54d1f0665b40c64a5d3bef55aad320d0444c637b
Shravani Allam Reddy <shravani.allam@gmail.com>
2020-09-16 11:59:22 -0600

    added comments to the code

--- script.js
+++ script.js
@@ -4,20 +4,30 @@ let currentTime;
 let eventData;
 let eventId;
 
+
+//To display the current day at the top of the calendar
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
-$(".coloriser").each(function () {
 
+//This below function adds color to all the timeblocks created in html 
+$(".coloriser").each(function () {
+    //this grabs the attribute 'id' from class 'timeblock' in class 'coloriser'
     time = $(this).find(".time-block").attr("id");
+    //momentjs time format to get the value of just 24hour time
     currentTime = moment().format("H");
+   
     // console.log(currentTime);
-    // console.log(time);
+    //If current time is greater than the time in timeblock , then we add class 'past' which is in style.css(grey color)
     if (currentTime > parseInt(time)) {
         $(this).find(".calendartext").addClass("past");
     }
+
+    //If current time is less than the time in timeblock , then we add class 'future' (green color)
     else if (currentTime < parseInt(time)) {
         $(this).find(".calendartext").addClass("future");
     }
+
+    //If current time is equal to the time in timeblock , then we add class 'present' (Red color)
     else if (currentTime == time) {
         $(this).find(".calendartext").addClass("present");
     }
@@ -25,17 +35,24 @@ $(".coloriser").each(function () {
 });
 
 
-
+// when save button is clicked , the below function gets executed
 $(".saveBtn").on("click", function (event) {
     event.preventDefault();
+
+    //this grabs the class 'coloriser' and finds class 'eventtext' and its value is stored in variable 'eventData'
     eventData = $(this).parent(".coloriser").find(".eventtext").val().trim();
+
+    //this sets the attribute 'id' from 'eventtext' class which is in 'coloriser' class to variable 'eventId'
     eventId = $(this).parent(".coloriser").find(".eventtext").attr("id");
     // console.log(eventData);
     // console.log(eventId);
+
+
+    //This stores the value of eventData in 'eventId' variable
     localStorage.setItem(eventId, eventData);
 })
 
-
+//This gets the value which is stored in 'eventId' variable which checks for each 'eventext' class and grabs the attribute 'id'
 $(".eventtext").each(function () {
     eventId = $(this).attr("id");
     $(this).val(localStorage.getItem(eventId));
